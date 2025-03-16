a48cbb721c0d3064559fa47ad39082b229b5d4dd
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-16 14:25:54 -0600

    Add tests for subtract function and update add tests to handle non-number types

--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,14 +1,21 @@
 import { describe, it, expect } from 'vitest';
 
-import { add } from './arithmetic';
+import { add, subtract, multiply, divide } from './arithmetic';
 
 describe('add', () => {
   it('should add two positive numbers', () => {
     expect(add(2, 3)).toBe(5);
   });
+  describe.skip('should NOT add non-number types', () => {
+    expect(add('hey', 'some'));
+  });
 });
 
-describe.todo('subtract', () => {});
+describe('subtract', () => {
+  it('should subtract two numbers', function () {
+    expect(subtract(2, 2)).toBe(0);
+  });
+});
 
 describe.todo('multiply', () => {});
 
