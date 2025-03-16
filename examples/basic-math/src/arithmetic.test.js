37cca03cea14de4a4aaae0250ca17c28a68208fc
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-16 14:21:09 -0600

    Add unit test for the add function

--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,6 +1,12 @@
 import { describe, it, expect } from 'vitest';
 
-describe.todo('add', () => {});
+import { add } from './arithmetic';
+
+describe('add', () => {
+  it('should add two positive numbers', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+});
 
 describe.todo('subtract', () => {});
 
