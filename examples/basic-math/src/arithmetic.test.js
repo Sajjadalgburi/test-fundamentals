44c94186baf26e42dea7aaed1244ac2bfbbb5643
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-16 14:27:13 -0600

    Add tests for multiply function and update add tests to handle non-number types

--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -6,6 +6,8 @@ describe('add', () => {
   it('should add two positive numbers', () => {
     expect(add(2, 3)).toBe(5);
   });
+
+  // Todo : Implement later
   describe.skip('should NOT add non-number types', () => {
     expect(add('hey', 'some'));
   });
@@ -17,6 +19,10 @@ describe('subtract', () => {
   });
 });
 
-describe.todo('multiply', () => {});
+describe('multiply', () => {
+  it('should multiply two numbers together', () => {
+    expect(multiply(3, 3)).toBe(9);
+  });
+});
 
 describe.todo('divide', () => {});
