2b908be2501890669e32b3f27810062beed00566
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-17 13:13:55 -0600

    Enhance arithmetic functions to handle string inputs and array subtraction

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,8 +1,23 @@
 export const add = (a, b) => {
+  if (typeof a === 'string') a = Number(a);
+  if (typeof b === 'string') b = Number(b);
+
+  if (isNaN(a) || isNaN(b))
+    throw new Error('Either Argument (a or b) is not a number');
+
   return a + b;
 };
 
 export const subtract = (a, b) => {
+  // [6, 2]
+  if (Array.isArray(a)) {
+    if (a.length > 2) throw new Error('Array Must be lenght two or less');
+    const index1 = a[0];
+    const index2 = a[1];
+    const total = index1 - index2;
+    return total - b;
+  }
+
   return a - b;
 };
 
