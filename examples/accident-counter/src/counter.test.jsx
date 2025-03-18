b122ac6ea9bee184bdf891a9ad232c2d4abccac3
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-18 14:06:27 -0600

    Enhance Counter component tests to cover all functionalities including increment, decrement, and reset behavior

--- examples/accident-counter/src/counter.test.jsx
+++ examples/accident-counter/src/counter.test.jsx
@@ -1,39 +1,97 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import { Counter } from './counter';
 
 import '@testing-library/jest-dom/vitest';
 
-describe.todo('Counter ', () => {
+describe('Counter ', () => {
   beforeEach(() => {
     render(<Counter />);
   });
 
-  it('renders with an initial count of 0');
+  it('renders with an initial count of 0', () => {
+    const counter = screen.getByTestId('counter-count');
+    expect(counter).toHaveTextContent('0');
+  });
 
-  it('disables the "Decrement" and "Reset" buttons when the count is 0');
+  it('disables the "Decrement" and "Reset" buttons when the count is 0', () => {
+    const resetCounter = screen.getByRole('button', { name: /reset/i });
+    const decrementCounter = screen.getByRole('button', { name: /decrement/i });
 
-  it.todo('displays "days" when the count is 0', () => {});
+    expect(resetCounter).toBeDisabled();
+    expect(decrementCounter).toBeDisabled();
+  });
 
-  it.todo(
-    'increments the count when the "Increment" button is clicked',
-    async () => {},
-  );
+  it('displays "days" when the count is 0', () => {
+    const counterUnit = screen.getByTestId('counter-unit');
 
-  it.todo('displays "day" when the count is 1', async () => {});
+    expect(counterUnit).toHaveTextContent('days');
+  });
 
-  it.todo(
-    'decrements the count when the "Decrement" button is clicked',
-    async () => {},
-  );
+  it('increments the count when the "Increment" button is clicked', async () => {
+    const counter = screen.getByTestId('counter-count'); // 0
+    const incrementButton = screen.getByRole('button', {
+      name: /increment/i,
+    });
 
-  it.todo('does not allow decrementing below 0', async () => {});
+    await userEvent.click(incrementButton);
+    await userEvent.click(incrementButton);
+    await userEvent.click(incrementButton);
 
-  it.todo(
-    'resets the count when the "Reset" button is clicked',
-    async () => {},
-  );
+    expect(counter).toHaveTextContent('3');
+  });
+
+  it('displays "day" when the count is 1', async () => {
+    const unitSpan = screen.getByTestId('counter-unit'); // 0
+    const incrementButton = screen.getByRole('button', { name: /increment/i });
+
+    await userEvent.click(incrementButton);
+    expect(unitSpan).toHaveTextContent('day');
+  });
+
+  it('decrements the count when the "Decrement" button is clicked', async () => {
+    const counter = screen.getByTestId('counter-count'); // 0
+    const incrementButton = screen.getByRole('button', {
+      name: /increment/i,
+    });
+    const decrementButton = screen.getByRole('button', {
+      name: /decrement/i,
+    });
+
+    await userEvent.click(incrementButton);
+    await userEvent.click(incrementButton); // 2 is displayed
+    await userEvent.click(decrementButton); // 1 is displayed
+
+    expect(counter).toHaveTextContent('1');
+  });
+
+  it('does not allow decrementing 0', async () => {
+    const counter = screen.getByTestId('counter-count'); // 0
+
+    const decrementButton = screen.getByRole('button', {
+      name: /decrement/i,
+    });
+    await userEvent.click(decrementButton);
+
+    expect(counter).toHaveTextContent('0');
+    expect(decrementButton).toBeDisabled();
+  });
+
+  it('resets the count when the "Reset" button is clicked', async () => {
+    const counter = screen.getByTestId('counter-count'); // 0
+    const incrementButton = screen.getByRole('button', {
+      name: /increment/i,
+    });
+    const resetButton = screen.getByRole('button', {
+      name: /reset/i,
+    });
+
+    await userEvent.click(incrementButton);
+    await userEvent.click(incrementButton);
+    await userEvent.click(incrementButton);
+    await userEvent.click(resetButton);
+    expect(counter).toHaveTextContent('0');
+  });
 
   it.todo(
     'disables the "Decrement" and "Reset" buttons when the count is 0',
