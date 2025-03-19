516c19091cd46432af6c92b76aa4a3817d5a76e7
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-19 14:52:58 -0600

    Add test for AlertButton to verify alert trigger on button click

--- examples/element-factory/src/alert-button.test.jsx
+++ examples/element-factory/src/alert-button.test.jsx
@@ -3,12 +3,33 @@ import userEvent from '@testing-library/user-event';
 
 import { AlertButton } from './alert-button';
 
-describe.todo('AlertButton', () => {
+describe('AlertButton', () => {
   beforeEach(() => {});
 
   afterEach(() => {});
 
   it('should render an alert button', async () => {});
 
-  it('should trigger an alert', async () => {});
+  it.only('should trigger an alert on click of the button', async () => {
+    /**
+     * This test should type in the input field and clcik on the button.
+     * We need to mock the button click to see if after clicked it shows the alret message.
+     */
+
+    // const alertSpy = vi.spyOn(window, 'alert');
+    const handleSubmit = vi.fn();
+
+    render(<AlertButton onSubmit={handleSubmit} initialMessage="Hello" />);
+    const input = screen.getByLabelText('Message');
+    const button = screen.getByRole('button', { name: /trigger/i });
+
+    await act(async () => {
+      await userEvent.clear(input);
+      await userEvent.type(input, 'Hello');
+      await userEvent.click(button);
+    });
+
+    expect(handleSubmit).toHaveBeenCalled();
+    expect(handleSubmit).toHaveBeenCalledWith('Hello');
+  });
 });
