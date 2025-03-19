c8c241186abbbfd8b5894a6c1dcb9bbdca90bf83
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-19 14:53:03 -0600

    Add tests for logger functionality in development mode

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,4 +1,23 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
-describe.todo('logger', () => {});
+describe('logger', () => {
+  describe('development', () => {
+    beforeEach(() => {
+      vi.stubEnv('MODE', 'development');
+    });
+
+    afterEach(() => {
+      vi.resetAllMocks();
+    });
+
+    it('logs to the console in development mode', () => {
+      const logSpy = vi.spyOn(console, 'log');
+
+      log('Hello');
+
+      expect(logSpy).toHaveBeenCalledOnce();
+      expect(logSpy).toHaveBeenCalledWith('Hello');
+    });
+  });
+});
